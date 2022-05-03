7dc278de385da5e2f7150045bff57e9194580ec2
iwaki-toshiyuki <toytoy0714@icloud.com>
2022-05-04 04:08:05 +0900

    i18nによる日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module NegativeWordShare
       g.helper false
       g.test_framework false
     end
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
