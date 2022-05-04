fb55a0a9b64c09ebb6892a959130053af2b1e41b
iwaki-toshiyuki <toytoy0714@icloud.com>
2022-05-04 17:10:37 +0900

    掲示板作成機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index]
+  resources :boards, only: %i[index new create]
 end
