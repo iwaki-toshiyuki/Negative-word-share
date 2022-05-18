2ac2c051ab2653c37ba17cfa5d53fdbf5fd396c3
iwaki-toshiyuki <toytoy0714@icloud.com>
2022-05-18 18:53:51 +0900

    dotenv-railsを導入

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,16 +1,15 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://negative-word-share.com/"
 SitemapGenerator::Sitemap.sitemaps_host = "https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
-SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
   ENV['S3_BUCKET_NAME'],
   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  aws_region: ENV['AWS_REGION'],
+  aws_region: 'ap-northeast-1'
 )
 
 SitemapGenerator::Sitemap.create do
-  
+
   # Put links creation logic here.
   #
   # The root path '/' and sitemap index file are added automatically for you.
